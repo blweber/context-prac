1651277d645adf1d88cbddebde80eae5aefda435
britweber <britweber@deloitte.com>
2019-09-29 21:14:35 -0400

    initial styling of layout components (sidebar, navbar, table)

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Container } from "reactstrap";
 
+import './layout.scss';
+
 const Layout = props => {
-  return <Container>{props.children}</Container>;
+  return <Container className={'container-div'}>{props.children}</Container>;
 };
 
 export default Layout;
