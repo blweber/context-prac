84f85ec23d60efe319e1a33a8c5d8c68542dc265
Billy Pheiffer <wpheiffer@deloitte.com>
2019-09-30 13:24:30 -0400

    material ui icons as well

--- client/src/layouts/Sidebar.js
+++ client/src/layouts/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Nav, NavItem, NavLink } from "reactstrap";
 import { Link } from "react-router-dom";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './sidebar.scss';
 
@@ -10,37 +15,95 @@ const Sidebar = () => {
       <Nav vertical>
         <NavItem className="sidebar-list">
           <NavLink href="#">
-            <Link to="/">
-              Dashboard
-            </Link>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography><b>Dashboard</b></Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Link to="/"><Typography>
+                  Dashboard Details
+          </Typography> </Link>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
           </NavLink>
         </NavItem>
         <NavItem className="sidebar-list">
           <NavLink href="#">
-            <Link to="/AdHoc">
-              Activities
-            </Link>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography><b>Activities</b></Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Link to="/AdHoc"><Typography>
+                  Activities Details
+                  </Typography> </Link>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
           </NavLink>
         </NavItem>
         <NavItem className="sidebar-list">
           <NavLink href="#">
-            <Link to="/Register">
-              Workflows
-            </Link>
+
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography><b>Workflows</b></Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Workflows Details
+          </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
           </NavLink>
         </NavItem>
         <NavItem className="sidebar-list">
           <NavLink href="#">
-            <Link to="/Login">
-              Geospatial
-            </Link>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography><b>Geospatial</b></Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  Geospatial Details
+          </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
           </NavLink>
         </NavItem>
         <NavItem className="sidebar-list">
           <NavLink href="#">
-            <Link to="/Register">
-              Configuration
-            </Link>
+            <ExpansionPanel>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography><b>Configuration</b></Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  <Link to="/">User Management </Link>
+                  <Link to="/systemroles">Manage System Roles</Link>
+          </Typography>
+               
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
           </NavLink>
         </NavItem>
       </Nav>
