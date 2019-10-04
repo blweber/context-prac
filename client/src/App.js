4c0db2244143c5717b2172991dc1d026d3af4578
britweber <britweber@deloitte.com>
2019-10-04 12:06:21 -0400

    updated app layout to Material Ui (styling)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 import Home from "./pages/Home";
 import Three33 from "./pages/Three33";
 import Three45 from "./pages/Three45";
 import AdHoc from "./pages/AdHoc";
 import PageNotFound from "./pages/PageNotFound";
-import "./App.css";
 import Layout from "../src/layouts/Layout";
-import { Navigationbar } from "./layouts/NavigationBar";
+import NavBar from "./layouts/NavBar";
 import Footer from "./layouts/Footer";
 import ContactForm from "./components/contacts/ContactForm";
 import Login from "./components/auth/Login";
@@ -17,37 +18,42 @@ import ContactState from "./context/contact/ContactState";
 import SystemRoles from "./pages/SystemRoles";
 import CreateSystemRole from "./pages/CreateSystemRole";
 
+import "./App.scss";
+
+import { StylesProvider } from "@material-ui/styles";
+import Grid from '@material-ui/core/Grid';
+
 const App = () => {
   return (
-    <ContactState>
-      <Fragment>
-        <Router>
-          <Navigationbar />
-          <Sidebar />
-          <Layout>
-            <div>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/Three33" component={Three33} />
-                <Route exact path="/Three45" component={Three45} />
-                <Route exact path="/AdHoc" component={AdHoc} />
-                <Route exact path="/SystemRoles" component={SystemRoles} />
-                <Route exact path="/ContactForm" component={ContactForm} />
-                <Route
-                  exact
-                  path="/CreateSystemRole"
-                  component={CreateSystemRole}
-                />
-                <Route exact path="/Login" component={Login} />
-                <Route exact path="/Register" component={Register} />
-                <Route path="/" component={PageNotFound} />
-              </Switch>
-            </div>
-          </Layout>
-          <Footer />
-        </Router>
-      </Fragment>
-    </ContactState>
+    <StylesProvider injectFirst>
+      <ContactState>
+        <Fragment>
+          <Router>
+            <NavBar />
+            <Grid container>
+              <Sidebar />
+              <Layout>
+                <div>
+                  <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/Three33" component={Three33} />
+                    <Route exact path="/Three45" component={Three45} />
+                    <Route exact path="/AdHoc" component={AdHoc} />
+                    <Route exact path="/SystemRoles" component={SystemRoles} />
+                    <Route exact path="/ContactForm" component={ContactForm} />
+                    <Route exact path="/CreateSystemRole" component={CreateSystemRole} />
+                    <Route exact path="/Login" component={Login} />
+                    <Route exact path="/Register" component={Register} />
+                    <Route path="/" component={PageNotFound} />
+                  </Switch>
+                </div>
+              </Layout>
+            </Grid>
+            <Footer />
+          </Router>
+        </Fragment>
+      </ContactState>
+    </StylesProvider>
   );
 };
 
