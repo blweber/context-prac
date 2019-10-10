c0a9f46c5cdad36917c48b76d25f7fd1c7461b26
britweber <britweber@deloitte.com>
2019-10-10 16:01:23 -0400

    removed dupe component import

--- client/src/components/activities/ActivityTable.js
+++ client/src/components/activities/ActivityTable.js
@@ -1,10 +1,10 @@
 import React, { useContext, useState } from "react";
+
 import IconButton from "@material-ui/core/IconButton";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Grid from "@material-ui/core/Grid";
-
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -13,7 +13,6 @@ import TableHead from '@material-ui/core/TableHead';
 // import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
-import IconButton from '@material-ui/core/IconButton';
 
 import ActivityContext from "../../context/activities/ActivityContext";
 import ActivitySearch from "./ActivitySearch";
